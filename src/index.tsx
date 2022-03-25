13980882b35fd6f0add55a382c8bf54835ca0c57
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-03-25 15:49:33 +0900

    format

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 // Inject some global styles
 mergeStyles({
-  ':global(body,html,#root)': {
-    margin: 0,
-    padding: 0,
-    height: '100vh',
-  },
+    ':global(body,html,#root)': {
+        margin: 0,
+        padding: 0,
+        height: '100vh',
+    },
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
