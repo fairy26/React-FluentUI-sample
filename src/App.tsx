04a534e5a610cb255ec874dac1242e969a991c48
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-03-25 15:52:03 +0900

    remove: 書き換え例

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import {
-    Stack,
-    Text,
-    Link,
-    FontWeights,
-    IStackTokens,
-    IStackStyles,
-    ITextStyles,
-} from '@fluentui/react';
-import logo from './logo.svg';
-import './App.css';
+import { Stack, Text, IStackTokens, IStackStyles } from '@fluentui/react';
 
-const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
 const stackTokens: IStackTokens = { childrenGap: 15 };
 const stackStyles: Partial<IStackStyles> = {
     root: {
@@ -31,38 +20,7 @@ export const App: React.FunctionComponent = () => {
             styles={stackStyles}
             tokens={stackTokens}
         >
-            <img className="App-logo" src={logo} alt="logo" />
-            <Text variant="xxLarge" styles={boldStyle}>
-                Welcome to your Fluent UI app
-            </Text>
-            <Text variant="large">
-                For a guide on how to customize this project, check out the Fluent UI documentation.
-            </Text>
-            <Text variant="large" styles={boldStyle}>
-                Essential links
-            </Text>
-            <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-                <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">
-                    Docs
-                </Link>
-                <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
-                    Stack Overflow
-                </Link>
-                <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-                <Link href="https://twitter.com/fluentui">Twitter</Link>
-            </Stack>
-            <Text variant="large" styles={boldStyle}>
-                Design system
-            </Text>
-            <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-                <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">
-                    Icons
-                </Link>
-                <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web">
-                    Styles
-                </Link>
-                <Link href="https://aka.ms/themedesigner">Theme designer</Link>
-            </Stack>
+            <Text variant="xxLarge">書き換えました</Text>
         </Stack>
     );
 };
