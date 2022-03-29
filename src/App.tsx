3f80e6e44a67cf88dee17e247fa9a7985a157d08
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-03-29 21:20:39 +0900

    add: useEffect, useMemo, useCallbackの処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import {
     Stack,
     Text,
@@ -20,40 +20,60 @@ const stackStyles: Partial<IStackStyles> = {
 };
 
 export const App: React.FunctionComponent = () => {
+    useEffect(() => {
+        console.log('App component mounted');
+        return () => {
+            console.log('App component unmounted');
+        };
+    }, []);
+
     const [todos, setTodos] = useState<string[]>([]);
     const [completedTodos, setCompletedTodos] = useState<string[]>([]);
     const [text, setText] = useState('');
 
-    const onInputChange = (_: any, newValue?: string) => {
+    const onInputChange = useCallback((_: any, newValue?: string) => {
         setText(newValue || '');
-    };
+    }, []);
 
-    const onClickAdd = () => {
+    const onClickAdd = useCallback(() => {
         if (text === '') return;
         const newTodos = [...todos, text];
         setTodos(newTodos);
         setText('');
-    };
+    }, [text, todos]);
 
-    const done = (index: number) => (_?: any, checked?: boolean) => {
-        if (checked) {
-            const newTodos = [...todos];
-            newTodos.splice(index, 1);
-            const newCompletedTodos = [...completedTodos, todos[index]];
-            setTodos(newTodos);
-            setCompletedTodos(newCompletedTodos);
-        }
-    };
+    const done = useCallback(
+        (index: number) => (_?: any, checked?: boolean) => {
+            if (checked) {
+                const newTodos = [...todos];
+                newTodos.splice(index, 1);
+                const newCompletedTodos = [...completedTodos, todos[index]];
+                setTodos(newTodos);
+                setCompletedTodos(newCompletedTodos);
+            }
+        },
+        [todos, completedTodos]
+    );
 
-    const reset = (index: number) => (_?: any, checked?: boolean) => {
-        if (!checked) {
-            const newCompletedTodos = [...completedTodos];
-            newCompletedTodos.splice(index, 1);
-            const newTodos = [...todos, completedTodos[index]];
-            setCompletedTodos(newCompletedTodos);
-            setTodos(newTodos);
-        }
-    };
+    const reset = useCallback(
+        (index: number) => (_?: any, checked?: boolean) => {
+            if (!checked) {
+                const newCompletedTodos = [...completedTodos];
+                newCompletedTodos.splice(index, 1);
+                const newTodos = [...todos, completedTodos[index]];
+                setCompletedTodos(newCompletedTodos);
+                setTodos(newTodos);
+            }
+        },
+        [todos, completedTodos]
+    );
+
+    const num = useMemo(() => todos.length, [todos]);
+    const numCompleted = useMemo(() => completedTodos.length, [completedTodos]);
+    const total = useMemo(() => num + numCompleted, [num, numCompleted]);
+    useEffect(() => {
+        console.log(`登録済み: ${total}`);
+    }, [total]);
 
     return (
         <Stack
@@ -76,6 +96,9 @@ export const App: React.FunctionComponent = () => {
             {completedTodos.map((todo, index) => {
                 return <Checkbox key={todo + index} label={todo} checked onChange={reset(index)} />;
             })}
+            <Text variant="xLarge">
+                {num > 0 ? `${num}個未消化です！` : numCompleted > 0 && '全部消化しました！'}
+            </Text>
         </Stack>
     );
 };
